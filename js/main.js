df74478b13fc56fbdb8ace8a0402ad2b86c7a2c4
Virlucero <virlucero@gmail.com>
2023-02-24 14:44:27 +0000

    completed search and list with images

--- js/main.js
+++ js/main.js
@@ -1,54 +1,53 @@
+"use strict";
 
-'use strict'
+// añado variables
 
-// añado variables  
-
-const inputElement = document.querySelector('.js-input')
-const searchButton = document.querySelector('.js-button-search')
-const resetButton = document.querySelector('.js-reset-button')
-const listElement = document.querySelector('.js-list')
+const inputElement = document.querySelector(".js-input");
+const searchButton = document.querySelector(".js-button-search");
+const resetButton = document.querySelector(".js-reset-button");
+const listElement = document.querySelector(".js-list");
 
 // creo funcion manejadora del boton buscar
-function searchHandler(evt){
-    evt.preventDefault();
-    //guardo lo que introduce el usuario 
-    let cocktailName=inputElement.value;
-    
-    // hago llamada al servidor para que me devuelva la lista de cockteles
-    
-    fetch (`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`)
-    .then ((response)=>response.json())
-    .then ((data)=> {
-            // recojo los cockteles introducidos con este let
-            let cocktails=data.drinks
-            for (const cocktail of cocktails) {
-                const newLi = renderCocktail(cocktail)
-                listElement.appendChild(newLi)
-            }
-        })
+function searchHandler(evt) {
+  evt.preventDefault();
+  //guardo lo que introduce el usuario
+  let cocktailName = inputElement.value;
+
+  // hago llamada al servidor para que me devuelva la lista de cockteles
+
+  fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // recojo los cockteles introducidos con este let
+      let cocktails = data.drinks;
+      for (const cocktail of cocktails) {
+        const newLi = renderCocktail(cocktail);
+        listElement.appendChild(newLi);
+      }
+    });
 }
 
-searchButton.addEventListener("click", searchHandler)
-
+searchButton.addEventListener("click", searchHandler);
 
 // creo funcion para que me pinte un coctel
+function renderCocktail(cocktailData) {
+  console.log(cocktailData);
+  const liElement = document.createElement("li");
+  const h4Title = document.createElement("h4");
+  const textForTitle = document.createTextNode(cocktailData.strDrink);
+  const img = document.createElement("img");
 
-function renderCocktail (cocktailData) {
+  let imageSource = cocktailData.strDrinkThumb;
+  if (imageSource === null) {
+    imageSource = "../images/default-cocktel.jpg";
+  }
+  img.src = imageSource;
+  img.classList.add("cocktail-img");
+  h4Title.appendChild(textForTitle);
+  liElement.appendChild(h4Title);
+  liElement.appendChild(img);
 
-    const liElement = document.createElement('li');
-    const h4Title = document.createElement('h4')
-    const textForTitle = document.createTextNode(cocktailData.strDrink);
-    h4Title.appendChild(textForTitle)
-    liElement.appendChild(h4Title)
-
-
-return liElement
-
-
+  return liElement;
 }
-
-
-
-
-
-    
